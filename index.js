feb06159feaa0c16327dcf21af971ec7c9144252
Catherine Li <catherine.uk.20@gmail.com>
2021-04-29 15:16:17 +0100

    data.js added

---
+++ index.js
@@ -0,0 +1,3 @@
+//   - Create an alert that lists all users, with their ids, names and what city they're from
+// - Prompt the user for a user id
+// - Display an alert with the username and all the todos titles that belong to that user 
